3f3e51c237e2a9d157fcfb892b1c146f2e2e2d74
Toxblh <toxblh@gmail.com>
2019-05-07 15:54:43 +0100

    init and done

---
+++ README.md
@@ -0,0 +1,17 @@
+# Collect credit score from clearscore.com
+A simple way to collect your score from clearscore.com
+
+### Example result of work
+```
+Report date: 2019-04-17
+Days until new: 12
+Score: 615
+```
+
+### Install
+1. `git clone https://github.com/Toxblh/ClearScore`
+2. `cd ClearScore`
+3. `echo "module.exports = { login: 'YourL0gin', pass: 'YourPassw0rd'}" > creds.js`
+4. `yarn` or `npm i`
+5. `node ./index.js`
+6. Your score already front of you
---
+++ index.js
@@ -0,0 +1,50 @@
+const puppeteer = require('puppeteer')
+const creds = require('./creds')
+
+async function getScore({ login, pass }) {
+  let userReport
+
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+
+  page.on('response', response => {
+    if (response.url() === 'https://app.clearscore.com/api/gb/report-viewer/v2/report') {
+      response.json().then(report => userReport = report)
+    }
+  });
+
+  await page.goto('https://app.clearscore.com/login', { waitUntil: 'networkidle2' })
+  await page.waitFor(1000)
+
+  await page.focus('input#loginform_email_input')
+  await page.keyboard.type(login)
+
+  await page.focus('input#loginform_password_input')
+  await page.keyboard.type(pass)
+
+  const inputElement = await page.$('button[type=submit]')
+  await inputElement.click()
+
+  await page.waitForNavigation()
+
+  await page.goto('https://app.clearscore.com/report', { waitUntil: 'networkidle2' })
+
+  const output = {
+    report_data: userReport.overview.reportDate,
+    updated_date: userReport.overview.daysUntilNextReport,
+    score: userReport.overview.score.score
+  }
+
+  await browser.close()
+
+  return output
+}
+
+getScore({
+  login: creds.login,
+  pass: creds.pass
+}).then(score => {
+  console.log('Report date:', score.report_data)
+  console.log('Days until new:', score.updated_date)
+  console.log('Score:', score.score)
+})
